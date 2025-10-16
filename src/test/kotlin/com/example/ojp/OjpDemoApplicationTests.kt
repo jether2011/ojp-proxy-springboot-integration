40e58c0d4743a8ea548990f4b6797e79d1b22565
Jether Rodrigues do Nascimento <jetherrodrigues@gmail.com>
2025-10-16 18:48:50 -0300

    feat: first structure for OJP Proxy integration

---
+++ src/test/kotlin/com/example/ojp/OjpDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ojp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OjpDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
